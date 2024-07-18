ea8214064e0f72a6bcd4261a9b6c8e5261959ceb
Ruzimurod11 <donievruzimurod@gmail.com>
2024-07-18 18:54:07 +0500

    finished portfolio part

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,8 +1,100 @@
 import React from "react";
 import "./portfolio.css";
+import IMG1 from "../../assets/portfolio1.png";
+import IMG2 from "../../assets/portfolio2.png";
+import IMG3 from "../../assets/portfolio3.png";
+import IMG4 from "../../assets/portfolio4.png";
+import IMG5 from "../../assets/portfolio5.png";
+import IMG6 from "../../assets/portfolio6.png";
+
+const data = [
+	{
+		id: 1,
+		image: IMG1,
+		title: "React Pizza",
+		github: "https://github.com/Ruzimurod11/react-pizza-ts",
+		demo: "https://react-pizza-rd.netlify.app",
+	},
+	{
+		id: 2,
+		image: IMG2,
+		title: "Posts",
+		github: "https://github.com/Ruzimurod11/React",
+		demo: "https://react-rd.netlify.app",
+	},
+	{
+		id: 3,
+		image: IMG3,
+		title: "Game Pokemon",
+		github: "https://github.com/Ruzimurod11/GamePokemon",
+		demo: "https://gamepokemon-rd.netlify.app",
+	},
+	{
+		id: 4,
+		image: IMG4,
+		title: "Random Users",
+		github: "https://github.com/Ruzimurod11/random-user",
+		demo: "ruzimurod11.github.io/random-user/",
+	},
+	{
+		id: 5,
+		image: IMG5,
+		title: "Weather",
+		github: "https://github.com/Ruzimurod11/weather",
+		demo: "https://ruzimurod11.github.io/weather/",
+	},
+	{
+		id: 6,
+		image: IMG6,
+		title: "Create Users",
+		github: "https://github.com/Ruzimurod11/create-users",
+		demo: "https://users-rd.netlify.app",
+	},
+];
 
 const Portfolio = () => {
-	return <div>Portfolio</div>;
+	return (
+		<section id="portfolio">
+			<h5>My Recent Work</h5>
+			<h2>Portfolio</h2>
+			<div className="container portfolio__container">
+				{data.map(({ id, image, title, github, demo }) => {
+					return (
+						<article
+							key={id}
+							className="portfolio__item"
+						>
+							<div>
+								<div className="portfolio__item-image">
+									<img
+										src={image}
+										alt="img"
+									/>
+								</div>
+								<h3>{title} </h3>
+							</div>
+							<div className="portfolio__item-cta">
+								<a
+									href={github}
+									className="btn"
+									target="__blank"
+								>
+									Github
+								</a>
+								<a
+									href={demo}
+									className="btn btn-primary"
+									target="__blank"
+								>
+									Live Demo
+								</a>
+							</div>
+						</article>
+					);
+				})}
+			</div>
+		</section>
+	);
 };
 
 export default Portfolio;
--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -2,7 +2,12 @@ import React from "react";
 import "./testimonials.css";
 
 const Testimonials = () => {
-	return <section id="testimonials">Testimonials</section>;
+	return (
+		<section id="testimonials">
+			<h5>Review from clients</h5>
+			<h2>Testimonials</h2>
+		</section>
+	);
 };
 
 export default Testimonials;
